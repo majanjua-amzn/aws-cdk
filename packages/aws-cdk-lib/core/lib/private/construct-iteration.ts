68bde3b03f8f83b57a1cb0c49f3c4a649d3cbefa
Rico Hermans <rix0rrr@gmail.com>
2025-05-26 13:16:07 +0200

    Update construct-iteration.ts
    Comments were outdated

--- packages/aws-cdk-lib/core/lib/private/construct-iteration.ts
+++ packages/aws-cdk-lib/core/lib/private/construct-iteration.ts
@@ -8,15 +8,10 @@ import { IConstruct } from 'constructs';
  * than this solution.
  */
 export function* iterateDfsPreorder(root: IConstruct) {
-  // Use a specialized queue data structure. Using `Array.shift()`
-  // has a huge performance penalty (difference on the order of
-  // ~50ms vs ~1s to iterate a large construct tree)
   const stack: IConstruct[] = [root];
 
   let next = stack.pop();
   while (next) {
-    // Get at the construct internals to get at the children faster
-    // const children: Record<string, IConstruct> = (next.construct.node as any)._children;
     for (const child of next.node.children) {
       stack.push(child);
     }
