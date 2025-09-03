6e649d28fe4b850fbbe94f225a4f3e8174562f6c
Abdelrahman Abdelrahman <abdlrmn@amazon.de>
2025-09-03 16:05:21 +0000

    ci(deployment-integ): add base commit option

--- tools/@aws-cdk/deployment-integ/lib/git.ts
+++ tools/@aws-cdk/deployment-integ/lib/git.ts
@@ -1,14 +1,14 @@
 import { spawn } from 'child_process';
 
 export const gitDiff = async (): Promise<string> => {
-  const process = spawn('git', ['diff', 'main', '--name-only'], {
+  const spawnProcess = spawn('git', ['diff', process.env.BASE_COMMIT ?? 'main', '--name-only'], {
     stdio: ['ignore', 'pipe', 'inherit'],
   });
 
   let output = '';
-  process.on('data', (data) => output += data);
+  spawnProcess.on('data', (data) => output += data);
 
-  return new Promise((resolve, reject) => process.on('close', (code) => {
+  return new Promise((resolve, reject) => spawnProcess.on('close', (code) => {
     if (code == 0) resolve(output);
     else reject(`git diff process failed with exit code ${code}`);
   }));
